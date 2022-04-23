92f205103cfe700f34ec771306f85fff2836a513
yys <yozx234@gmail.com>
2022-04-24 01:38:32 +0900

    first commit

--- domain/src/main/kotlin/com/yuk/domain/book/EBook.kt
+++
@@ -1,8 +0,0 @@
-package com.yuk.domain.book
-
-import javax.persistence.DiscriminatorValue
-import javax.persistence.Entity
-
-@Entity
-@DiscriminatorValue("2")
-class EBook : Book()
--- domain/src/main/kotlin/com/yuk/domain/review/Reviews.kt
+++
@@ -1,7 +0,0 @@
-package com.yuk.domain.review
-
-interface Reviews {
-    fun getReviews(): List<Review>
-    fun findById(reviewId: ReviewId): Review?
-    fun addReview(review: Review)
-}
